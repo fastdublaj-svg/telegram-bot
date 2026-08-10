61a05295f4c398532582c58f2db05edd3da2c289
fastdublaj-svg <fastdublaj@gmail.com>
2026-08-10 09:12:27 +0500

    FastBot1.py

--- FastBot1.py
+++ FastBot1.py
@@ -116,10 +116,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-un(main())
-ot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
- __name__ == "__main__":
-    asyncio.run(main())
